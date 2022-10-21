11c05ea8b323b1d74de96415f727b0e1614a01a7
Riadh <riadh.azzoun@hotmail.com>
2022-10-21 10:51:01 +0100

    Added readme

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from bbc_scraper import NewsScraper
 
 if __name__ == "__main__":
     scraper = NewsScraper()
-    scraper.save_news(q="Trump", filename='news', save_path="./news")
+    scraper.save_news(q="News", pages=1, filename='news', save_path="./news")
